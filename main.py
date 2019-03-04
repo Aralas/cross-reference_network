40868f61aa575f5ad5b93dc7bee0f2f989876b76
Aralas <14300180119@fudan.edu.cn>
2019-03-04 15:52:14 +0800

    solve OOM

---
+++ main.py
@@ -0,0 +1,61 @@
+# -*- coding:utf-8 -*-
+"""
+@author:Xu Jingyi
+@file:main.py.py
+@time:2019-03-0413:28
+"""
+import psutil
+import os
+import RunTest6
+
+file_index = 1
+
+RunTest6.initialization(1)
+
+info = psutil.virtual_memory()
+print(u'内存使用：', psutil.Process(os.getpid()).memory_info().rss)
+print(u'总内存：', info.total)
+print(u'内存占比：', info.percent)
+print(u'cpu个数：', psutil.cpu_count())
+
+for key in list(globals().keys()):
+    if not key.startswith("__"):
+        globals().pop(key)
+
+import psutil
+import os
+
+info = psutil.virtual_memory()
+print(u'清除所有变量')
+print(u'内存使用：', psutil.Process(os.getpid()).memory_info().rss)
+print(u'总内存：', info.total)
+print(u'内存占比：', info.percent)
+print(u'cpu个数：', psutil.cpu_count())
+
+for block in range(5):
+    import RunTest6
+    file_index = 1
+
+    start_section = 5 * (block + 1)
+    end_section = 5 * (block + 2)
+    RunTest6.run_cross_reference(start_section, end_section, file_index)
+
+    info = psutil.virtual_memory()
+    print(u'内存使用：', psutil.Process(os.getpid()).memory_info().rss)
+    print(u'总内存：', info.total)
+    print(u'内存占比：', info.percent)
+    print(u'cpu个数：', psutil.cpu_count())
+
+    for key in list(globals().keys()):
+        if not key.startswith("__"):
+            globals().pop(key)
+
+    import psutil
+    import os
+
+    info = psutil.virtual_memory()
+    print(u'清除所有变量')
+    print(u'内存使用：', psutil.Process(os.getpid()).memory_info().rss)
+    print(u'总内存：', info.total)
+    print(u'内存占比：', info.percent)
+    print(u'cpu个数：', psutil.cpu_count())
